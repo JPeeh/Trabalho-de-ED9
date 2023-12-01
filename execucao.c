9d6c20e71a222a57f8e03c373814d06301067963
JPeeh <jphsouza1234@gmail.com>
2023-12-01 18:03:22 -0300

    trabalho9

---
+++ execucao.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define MAX_SIZE 100
+
+typedef struct {
+    float stack[MAX_SIZE];
+    int top;
+} Stack;
+
+void push(Stack *s, float value) {
+    if (s->top == MAX_SIZE - 1) {
+        printf("Erro: Pilha cheia\n");
+        exit(EXIT_FAILURE);
+    }
+    s->top++;
+    s->stack[s->top] = value;
+}
+
+float pop(Stack *s) {
+    if (s->top == -1) {
+        printf("Erro: Pilha vazia\n");
+        exit(EXIT_FAILURE);
+    }
+    float value = s->stack[s->top];
+    s->top--;
+    return value;
+}
+
+float evaluateExpression(char *expression) {
+    Stack s;
+    s.top = -1;
+
+    char *token = strtok(expression, " ");
+    while (token != NULL) {
+        if (isdigit(token[0]) || (token[0] == '-' && isdigit(token[1]))) {
+            push(&s, atof(token));
+        } else if (token[0] == '+' || token[0] == '-' || token[0] == '*' || token[0] == '/') {
+            float b = pop(&s);
+            float a = pop(&s);
+            switch (token[0]) {
+                case '+':
+                    push(&s, a + b);
+                    break;
+                case '-':
+                    push(&s, a - b);
+                    break;
+                case '*':
+                    push(&s, a * b);
+                    break;
+                case '/':
+                    push(&s, a / b);
+                    break;
+            }
+        } else if (strcmp(token, "^") == 0) {
+            float b = pop(&s);
+            float a = pop(&s);
+            push(&s, pow(a, b));
+        } else if (strcmp(token, "sqrt") == 0) {
+            float a = pop(&s);
+            push(&s, sqrt(a));
+        } else {
+            printf("Operação não suportada: %s\n", token);
+            exit(EXIT_FAILURE);
+        }
+
+        token = strtok(NULL, " ");
+    }
+
+    return pop(&s);
+}
+
+int main() {
+    char expression[MAX_SIZE];
+    
+    printf("Digite a expressão em notação posfixa: ");
+    fgets(expression, MAX_SIZE, stdin);
+
+    float result = evaluateExpression(expression);
+    printf("Resultado: %.2f\n", result);
+
+    return 0;
+}
+
+
+/*Digite os valores: dando espaço entre todos os caracteres.*/
